919523f37635744a32e476cee1af6d11659d2179
root <1787191649@qq.com>
2017-06-15 10:57:41 +0800

    获取省市县的数据信息

--- app/src/main/java/com/coolweather/android/MainActivity.java
+++ app/src/main/java/com/coolweather/android/MainActivity.java
@@ -1,17 +1,24 @@
 package com.coolweather.android;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    public LocationClient mLocationClient;
+    private TextView positionText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mLocationClient = new LocationClient(getApplicationContext());
+        mLocationClient.registerLocationListener(new MyLocationListener());
         setContentView(R.layout.activity_main);
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         if (prefs.getString("weather", null) != null) {
@@ -19,6 +26,20 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
             finish();
         }
+        positionText = (TextView) findViewById(R.id.position_text_view);
+        List<String> permissionList = new ArrayList<>();
+
+        
     }
 
+    private class LocationClient {
+        public LocationClient(Context applicationContext) {
+        }
+
+        public void registerLocationListener(MyLocationListener myLocationListener) {
+        }
+    }
+
+    private class MyLocationListener {
+    }
 }
